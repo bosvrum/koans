b9d4e4560d9cb100a561c50adcff0829ae5f499f
Catalin <catalinboruga@yahoo.com>
2014-08-30 12:14:04 +0200

    Add pins/user association

--- Documents/Catalin/learn_programming/Carti/Ruby/books_pass/learn_to_programm/chapter8.rb
+++ Documents/Catalin/learn_programming/Carti/Ruby/books_pass/learn_to_programm/chapter8.rb
@@ -19,18 +19,59 @@ puts foods.join(' :) ') + ' 8)'
 200.times do puts []
 end
 
-puts 'Task as many words do you want!'
-words = []
 
+
+
+puts 'Give me some words, and I will sort them:' 
+words = []
 while true
-	word = gets.chomp
-	if word == ''
-		break
-	end
-	words.push word
+word = gets.chomp 
+break if word.empty?
+
+words << word
 end
 
-puts 'Sweet! Here they are, sorted:'
+puts 'Sweet! Here they are, sorted:' 
 puts words.sort
+
+
+title = 'Table of Contents'
+chapters = [['Getting Started', 1], 
+			['Numbers', 9], 
+			['Letters', 13]]
+puts title.center(50)
+puts
+
+chap_num = 1 
+chapters.each do |chap|
+name = chap[0]
+page = chap[1]
+beginning = 'Chapter ' + chap_num.to_s + ': ' + name 
+ending = 'page ' + page.to_s
+puts beginning.ljust(30) + ending.rjust(20)
+chap_num = chap_num + 1
+end	
+
+
+
+
+
+title = 'Table of Contents'
+chapters = [['Getting Started', 1], 
+			['Numbers', 9], 
+			['Letters', 13]]
+puts title.center(50)
+puts
+chapters.each_with_index do |chap, idx|
+  name, page = chap
+  chap_num   = idx + 1
+  beginning = "Chapter #{chap_num}:  #{name}"
+  ending    = "page #{page}"
+
+  puts beginning.ljust(30) + ending.rjust(20)
+end
+￼￼￼
+
+
 
 
